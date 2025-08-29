fdcffc7dcb408965eee002bad42a5ffa7be081b8
amankhan2005 <amankhan099k@gmail.com>
2025-08-29 15:43:29 +0530

    footer-align

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -14,7 +14,7 @@ const handletop = () => {
 const Footer = () => {
   return (
     <>
-      <footer className="bg-primary text-white py-8 border-b">
+      <footer className="bg-primary text-white py-10 border-b">
         <div className=" container mx-auto px-4 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 md:gap-2 gap-8">
 
