37a30c0c75fe65a99e117a2c8529d44a634c685a
amankhan2005 <amankhan099k@gmail.com>
2025-09-02 11:18:48 +0530

    changes

--- AdminPannel/src/Components/AppointmentData.jsx
+++ AdminPannel/src/Components/AppointmentData.jsx
@@ -163,7 +163,7 @@ const AppointmentData = () => {
       {loadCount < appointments.length && (
         <div className="text-center mt-6">
           <button
-            className="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-6 py-2 rounded shadow transition"
+            className="bg-blue-700 hover:bg-blue-700 text-white font-semibold px-6 py-2 rounded shadow transition"
             onClick={onMore}
           >
             Show More
